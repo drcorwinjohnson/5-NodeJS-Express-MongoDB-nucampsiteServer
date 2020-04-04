6a212a7e2c5bdb1e478e01325591675741b0810f
Corwin Johnson <corwin.t.johnson@gmail.com>
2020-04-03 20:40:26 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '490445238550191',
+        clientSecret: '446777ec58b2471561ef1c36c2165e95'
+    }
 }
